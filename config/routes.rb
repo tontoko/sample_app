f1f975be6e857ddac77cfb33f78d29d5496bbc8e
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2019-03-04 14:39:57 +0000

    12.6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
@@ -12,5 +14,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
 end
