10624bc34a943da864e38f2435771141224f571a
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2019-03-08 14:19:47 +0000

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'sessions/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -14,6 +11,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,7 +21,7 @@ class UsdersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
+      post signup_path, params: { user: { name:  "Example User",
                                          email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
