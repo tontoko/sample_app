a55749b9aed2a854464f575ef6a902d1b44b42e4
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2019-03-09 12:10:26 +0000

    13.3.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_143841) do
+ActiveRecord::Schema.define(version: 2019_03_09_104948) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
