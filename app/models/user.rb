fc6f11bbf1ff8fccfc06206a8221bec9ab397718
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2019-03-18 13:58:01 +0000

    14.1.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
@@ -91,6 +95,19 @@ class User < ApplicationRecord
     
     def feed
         Micropost.where("user_id = ?", id) # SQLインジェクション対策 = ? でエスケープ
+    end
+    
+    # Follow関係
+    def follow(other_user)
+        following << other_user # 配列の最後に新しく追加する
+    end
+    
+    def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+    
+    def following?(other_user)
+        following.include?(other_user)
     end
 
     private
